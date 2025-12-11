7878415922773499105faf367c7bc87411cf5acf
Judson Paiva <judson.paiva@hotmail.com>
2025-12-11 14:22:56 +0100

    criacao do projecto - com Lombok, Spring Web, Spring Boot, Spring Data JPA, H2 Database

---
+++ src/main/java/com/github/paivajudson/Produtos/Demo/ProdutosDemoApplication.java
@@ -0,0 +1,12 @@
+package com.github.paivajudson.Produtos.Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutosDemoApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(ProdutosDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/paivajudson/Produtos/Demo/ProdutosDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.paivajudson.Produtos.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
