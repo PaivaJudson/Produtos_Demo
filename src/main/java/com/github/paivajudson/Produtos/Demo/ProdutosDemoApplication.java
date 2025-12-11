faa945ab4f2f31092f260118d26abd47e54e6858
Judson Paiva <judson.paiva@hotmail.com>
2025-12-11 23:15:13 +0100

    teste com hello-world

--- src/main/java/com/github/paivajudson/Produtos/Demo/ProdutosDemoApplication.java
+++ src/main/java/com/github/paivajudson/Produtos/Demo/ProdutosDemoApplication.java
@@ -2,9 +2,17 @@ package com.github.paivajudson.Produtos.Demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ProdutosDemoApplication {
+
+    @GetMapping("/hello-world")
+    public String helloWorld(){
+        return "Hello World!";
+    }
 
 	public static void main(String[] args) {
         SpringApplication.run(ProdutosDemoApplication.class, args);
