0651399cc4bb1e9e23d9623cf93a4f38743d5624
Judson Paiva <judson.paiva@hotmail.com>
2025-12-15 01:28:45 +0100

    deletar produtos com DELETE

--- src/main/java/com/github/paivajudson/Produtos/Demo/controller/ProdutoController.java
+++ src/main/java/com/github/paivajudson/Produtos/Demo/controller/ProdutoController.java
@@ -35,6 +35,14 @@ public class ProdutoController {
         return produtoRepository.findById(id).orElse(null);
     }
 
+
+    @DeleteMapping("{id}")
+    public void deletar(@PathVariable("id") String id){
+        produtoRepository.deleteById(id);
+    }
+
+
+
 }
 
 
